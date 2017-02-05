ed41fc5cad53f47ed619be45ab385e588ef100b7
Andreas Kloeckner <inform@tiker.net>
2017-02-05 16:15:11 -0600

    Use as-needed shared lib deps

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,7 @@ cmake \
   -D HAVE_CLOCK_GETTIME=1 \
   -D KERNELLIB_HOST_CPU_VARIANTS=distro \
   -D OPENCL_LIBRARIES="-L${PREFIX}/lib;OpenCL" \
+  -D EXTRA_HOST_LD_FLAGS="-Wl,--as-needed" \
   ..
 
 make -j 8
