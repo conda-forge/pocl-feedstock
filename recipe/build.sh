768001420477c86264a25e102e9be725dc45dd9e
Matt Wala <wala1@illinois.edu>
2017-02-10 15:03:47 -0600

    OSX build: set HAVE_CLOCK_GETTIME=0

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,15 +3,17 @@ cd build
 
 if [ "$(uname)" == "Darwin" ]; then
   OPENCL_LIBRARIES=""
-else
+  HAVE_CLOCK_GETTIME=0
+else  # linux for now
   OPENCL_LIBRARIES="-L${PREFIX}/lib;OpenCL"
+  HAVE_CLOCK_GETTIME=1
 fi
 
 cmake \
   -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
   -D POCL_INSTALL_ICD_VENDORDIR="${PREFIX}/etc/OpenCL/vendors" \
   -D LLVM_CONFIG="${PREFIX}/bin/llvm-config" \
-  -D HAVE_CLOCK_GETTIME=1 \
+  -D HAVE_CLOCK_GETTIME="${HAVE_CLOCK_GETTIME}" \
   -D KERNELLIB_HOST_CPU_VARIANTS=distro \
   -D OPENCL_LIBRARIES="${OPENCL_LIBRARIES}" \
   -D EXTRA_HOST_LD_FLAGS="-Wl,--as-needed" \
