aba3a005bcab5d40f5bd9ec4315fcc55ca9b893f
Andreas Kloeckner <inform@tiker.net>
2017-02-11 18:15:24 -0600

    Install OpenCL headers on OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,11 @@ cd build
 if [ "$(uname)" == "Darwin" ]; then
   OPENCL_LIBRARIES=""
   HAVE_CLOCK_GETTIME=0
+  INSTALL_OPENCL_HEADERS=ON
 else  # linux for now
   OPENCL_LIBRARIES="-L${PREFIX}/lib;OpenCL"
   HAVE_CLOCK_GETTIME=1
+  INSTALL_OPENCL_HEADERS=OFF
 fi
 
 cmake \
@@ -14,6 +16,7 @@ cmake \
   -D POCL_INSTALL_ICD_VENDORDIR="${PREFIX}/etc/OpenCL/vendors" \
   -D LLVM_CONFIG="${PREFIX}/bin/llvm-config" \
   -D HAVE_CLOCK_GETTIME="${HAVE_CLOCK_GETTIME}" \
+  -D INSTALL_OPENCL_HEADERS="${INSTALL_OPENCL_HEADERS}" \
   -D KERNELLIB_HOST_CPU_VARIANTS=distro \
   -D OPENCL_LIBRARIES="${OPENCL_LIBRARIES}" \
   -D EXTRA_HOST_LD_FLAGS="-Wl,--as-needed" \
