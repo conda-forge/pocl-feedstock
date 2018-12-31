e0b7103ca17eb615abf695c3f28f5d48a11e6dfc
Isuru Fernando <isuruf@gmail.com>
2018-12-31 06:01:06 -0600

    Use anaconda compilers on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,9 +9,13 @@ if [ "$(uname)" == "Darwin" ]; then
 else  # linux for now
   OPENCL_LIBRARIES="-L${PREFIX}/lib;OpenCL"
   INSTALL_OPENCL_HEADERS=OFF
-  LINKER_FLAG="-D LINK_COMMAND=llvm-lld"
+  LINKER_FLAG="-D LINK_COMMAND=$LD"
   EXTRA_HOST_LD_FLAGS="--as-needed"
-  export LDFLAGS="$LDFLAGS -fuse-ld=lld --sysroot=$BUILD_PREFIX/$HOST/sysroot/usr"
+fi
+
+if [[ "$(uname)" == "Darwin" || "$c_compiler" == "toolchain_c" ]]; then
+  export CC=$PREFIX/bin/clang
+  export CXX=$PREFIX/bin/clang++
 fi
 
 cmake \
@@ -19,8 +23,6 @@ cmake \
   -D CMAKE_INSTALL_PREFIX="${PREFIX}" \
   -D POCL_INSTALL_ICD_VENDORDIR="${PREFIX}/etc/OpenCL/vendors" \
   -D LLVM_CONFIG="${PREFIX}/bin/llvm-config" \
-  -D CMAKE_C_COMPILER=${PREFIX}/bin/clang \
-  -D CMAKE_CXX_COMPILER=${PREFIX}/bin/clang++ \
   -D INSTALL_OPENCL_HEADERS="${INSTALL_OPENCL_HEADERS}" \
   -D KERNELLIB_HOST_CPU_VARIANTS=distro \
   -D OPENCL_LIBRARIES="${OPENCL_LIBRARIES}" \
