4aea2e7b758e0009f654b7a9242b9e8e4f81114e
Isuru Fernando <isuruf@gmail.com>
2025-12-06 17:46:29 -0800

    fix windows path sep

--- recipe/install-pkg.bat
+++ recipe/install-pkg.bat
@@ -4,23 +4,24 @@ SetLocal EnableDelayedExpansion
 set "PREFIX=%LIBRARY_PREFIX%"
 set "HOST=x86_64-pc-windows-msvc"
 
-cd build/pkgs
-mkdir -p %PREFIX%/bin/pocl
-mkdir -p %PREFIX%/share/pocl
+cd build\pkgs
+mkdir -p %PREFIX%\bin\pocl
+mkdir -p %PREFIX%\share\pocl
+
+dir
 
 if "%PKG_NAME%" == "pocl-cuda" (
-  mv pocl-devices-cuda.dll %PREFIX%/bin/pocl/
-  mv kernel-nvptx64-*.bc %PREFIX%/share/pocl/
-  mv cuda %PREFIX%/share/pocl/
+  move pocl-devices-cuda.dll %PREFIX%\bin\pocl\
+  move kernel-nvptx64-*.bc %PREFIX%\share\pocl\
+  move cuda %PREFIX%\share\pocl\
 )
 if "%PKG_NAME%" == "pocl-cpu-minimal" (
-  mv pocl-devices-basic.dll %PREFIX%/bin/pocl/
-  ls kernel-%HOST%*.bc
-  mv kernel-%HOST%*.bc %PREFIX%/share/pocl/
+  move pocl-devices-basic.dll %PREFIX%\bin\pocl\
+  move kernel-%HOST%*.bc %PREFIX%\share\pocl\
 )
 if "%PKG_NAME%" == "pocl-cpu" (
-  mv pocl-devices-pthread.dll %PREFIX%/bin/pocl/
+  move pocl-devices-pthread.dll %PREFIX%\bin\pocl\
 )
 if "%PKG_NAME%" == "pocl-remote" (
-  mv pocl-devices-remote.dll %PREFIX%/bin/pocl/
+  move pocl-devices-remote.dll %PREFIX%\bin\pocl\
 fi
